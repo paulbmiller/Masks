9e9a7673530d75c10a5cf727ae27ff3bc7c86a53
Paul Miller <miller_p@hotmail.fr>
2020-02-24 16:13:22 +0100

    Implemented different kernels for edge detection, 5x5 kernels and the ability to save images

--- main.py
+++ main.py
@@ -64,19 +64,33 @@ def change_channels(image, red=1, green=1, blue=1):
     return image
 
 
-def apply_conv(image, kernel):
-    """Applies a 3x3 filter on the image."""
+def apply_conv(image, kernel, grey=False):
+    """Applies a convolutional filter on the image."""
     weights = kernel.return_weights()
     image = np.expand_dims(image, axis=0)
+    if grey:
+        image = np.expand_dims(image, axis=3)
     image = np.transpose(image, (3, 0, 1, 2))
     image = torch.DoubleTensor(image)
     weights = torch.DoubleTensor(weights)
-    weights = weights.view(1, 1, 3, 3).repeat(1, 1, 1, 1)
-    output = F.conv2d(image, weights, padding=1).numpy()
+    if kernel.size == 2:
+        padx = image.shape[0] % 2
+        pady = image.shape[1] % 2
+    elif kernel.size == 3:
+        padx = 1
+        pady = 1
+    elif kernel.size == 5:
+        padx = 2
+        pady = 2
+    weights = weights.view(1, 1, kernel.size, kernel.size).repeat(1, 1, 1, 1)
+    output = F.conv2d(image, weights, padding=(padx, pady)).numpy()
     output = output.astype(int)
     output = np.transpose(output, (1, 2, 3, 0))[0]
+    output = np.clip(output, 0, 255)
+    if grey:
+        output = np.squeeze(output, axis=2)
 
-    return np.clip(output, 0, 255)
+    return output
 
 
 def apply_blur(image):
@@ -89,11 +103,28 @@ def apply_gaussianblur(image):
     return apply_conv(image, weights)
 
 
+def corrected_grey(pixel):
+    return 0.2126*pixel[0] + 0.7152*pixel[1] + 0.0722*pixel[2]
+
+
 class Kernel():
-    def __init__(self, weights=np.array([[0, 0, 0],[0, 1, 0],[0, 0, 0]]),
+    def __init__(self, weights=np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]]),
                  coeff=1.):
         self.weights = weights
         self.coeff = coeff
+        self.size = self.weights.shape[0]
+
+    def to_5x5(self):
+        if self.size == 5:
+            return self.weights
+        elif self.size == 3:
+            w = np.zeros((5, 5))
+            w[1:4, 1:4] = self.weights
+            return w
+        elif self.size == 2:
+            w = np.zeros((5, 5))
+            w[2:4, 2:4] = self.weights
+            return w
 
     def return_weights(self):
         return self.coeff * self.weights
@@ -101,6 +132,7 @@ class Kernel():
 
 class Display():
     def __init__(self, filename):
+        self.filename = filename
         self.image = load_image(filename)
         self.img_nochannel_change = np.copy(self.image)
         self.root = tk.Tk()
@@ -110,6 +142,7 @@ class Display():
         self.a = f.add_axes([0, 0, 1, 1])
         self.a.imshow(self.image)
         self.a.axis('off')
+        self.grey = False
 
         self.canvas = FigureCanvasTkAgg(f, self.root)
         self.canvas.draw()
@@ -127,33 +160,95 @@ class Display():
 
         self.button_edgedetect = tk.Button(self.root, command=self.edgedetect,
                                            text='Edge')
-        self.button_edgedetect.grid(row=0, column=3)
+        self.button_edgedetect.grid(row=0, column=5)
 
         self.button_sharpen = tk.Button(self.root, command=self.sharpen,
                                         text='Sharpen')
-        self.button_sharpen.grid(row=1, column=3)
+        self.button_sharpen.grid(row=0, column=3)
 
         self.button_sobel = tk.Button(self.root, command=self.sobel,
                                       text='Sobel')
-        self.button_sobel.grid(row=0, column=4)
+        self.button_sobel.grid(row=1, column=5)
+
+        self.button_grey = tk.Button(self.root, command=self.to_grey,
+                                     text='Grey')
+        self.button_grey.grid(row=0, column=4)
+
+        self.button_corr_grey = tk.Button(self.root,
+                                          command=self.to_corrected_grey,
+                                          text='Corr. grey')
+        self.button_corr_grey.grid(row=1, column=4)
+
+        self.button_canny = tk.Button(self.root, command=self.gaussian_filter,
+                                      text='Gaussian filter')
+        self.button_canny.grid(row=2, column=2)
+
+        self.button_prewitt = tk.Button(self.root, command=self.prewitt,
+                                        text='Prewitt')
+        self.button_prewitt.grid(row=2, column=5)
+
+        self.button_roberts = tk.Button(self.root, command=self.roberts,
+                                        text='Roberts')
+        self.button_roberts.grid(row=0, column=6)
 
         self.entries = self.init_entries()
 
         self.main_mult = tk.Entry(self.root)
         self.main_mult.insert(0, '1')
-        self.main_mult.grid(row=6, column=3)
+        self.main_mult.grid(row=8, column=4)
 
         self.button_custom = tk.Button(self.root, command=self.custom_kernel,
-                                       text='Apply custom kernel')
-        self.button_custom.grid(row=7, column=3)
-        
+                                       text='Apply kernel')
+        self.button_custom.grid(row=8, column=3)
+
+        self.entry_file = tk.Entry(self.root)
+        self.entry_file.grid(row=9, column=4)
+        self.entry_file.insert(0, self.filename)
+
+        self.button_load_img = tk.Button(self.root, command=self.load_img,
+                                         text='Load image')
+        self.button_load_img.grid(row=9, column=5)
+
         self.button_reset_img = tk.Button(self.root, command=self.reset_image,
                                           text='Reset image')
-        self.button_reset_img.grid(row=8, column=2)
+        self.button_reset_img.grid(row=9, column=3)
 
         self.button_reset_k = tk.Button(self.root, command=self.reset_kernel,
                                         text='Reset kernel')
-        self.button_reset_k.grid(row=8, column=4)
+        self.button_reset_k.grid(row=8, column=5)
+
+        self.button_swap021 = tk.Button(self.root,
+                                        command=lambda: self.swap('021'),
+                                        text='RBG')
+        self.button_swap021.grid(row=10, column=2)
+
+        self.button_swap120 = tk.Button(self.root,
+                                        command=lambda: self.swap('120'),
+                                        text='GBR')
+        self.button_swap120.grid(row=10, column=3)
+
+        self.button_swap102 = tk.Button(self.root,
+                                        command=lambda: self.swap('102'),
+                                        text='GRB')
+        self.button_swap102.grid(row=10, column=4)
+
+        self.button_swap201 = tk.Button(self.root,
+                                        command=lambda: self.swap('201'),
+                                        text='BRG')
+        self.button_swap201.grid(row=10, column=5)
+
+        self.button_swap210 = tk.Button(self.root,
+                                        command=lambda: self.swap('210'),
+                                        text='BGR')
+        self.button_swap210.grid(row=10, column=6)
+
+        self.button_saveas = tk.Button(self.root,
+                                       command=self.save_img,
+                                       text='Save image as')
+        self.button_saveas.grid(row=11, column=3)
+
+        self.entry_saveas = tk.Entry(self.root)
+        self.entry_saveas.grid(row=11, column=4)
 
         self.button_red = tk.Button(self.root, command=self.reset_red,
                                     text='Reset', bg='red')
@@ -186,75 +281,194 @@ class Display():
 
         self.root.mainloop()
 
+    def swap(self, order):
+        if order == '021':
+            col1 = self.image[:, :, 1].copy()
+            col2 = self.image[:, :, 2].copy()
+            self.image[:, :, 1], self.image[:, :, 2] = col2, col1
+        elif order == '120':
+            col0 = self.image[:, :, 0].copy()
+            col1 = self.image[:, :, 1].copy()
+            col2 = self.image[:, :, 2].copy()
+            self.image[:, :, 0], self.image[:, :, 1], self.image[:, :, 2] =\
+                col1, col2, col0
+        elif order == '102':
+            col0 = self.image[:, :, 0].copy()
+            col1 = self.image[:, :, 1].copy()
+            self.image[:, :, 0], self.image[:, :, 1] = col1, col0
+        elif order == '201':
+            col0 = self.image[:, :, 0].copy()
+            col1 = self.image[:, :, 1].copy()
+            col2 = self.image[:, :, 2].copy()
+            self.image[:, :, 0], self.image[:, :, 1], self.image[:, :, 2] =\
+                col2, col0, col1
+        elif order == '210':
+            col0 = self.image[:, :, 0].copy()
+            col2 = self.image[:, :, 2].copy()
+            self.image[:, :, 0], self.image[:, :, 2] = col2, col0
+        self.redraw(self.grey)
+
     def blur(self):
         kernel = Kernel(np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]), 1/9)
         self.update_kernel(kernel)
-        self.image = apply_conv(self.image, kernel)
-        self.redraw()
+        self.image = apply_conv(self.image, kernel, self.grey)
+        self.redraw(self.grey)
+
+    def gaussian_filter(self):
+        x_kern_weights = np.array([[2, 4, 5, 4, 2], [4, 9, 12, 9, 4],
+                                  [5, 12, 15, 12, 5], [4, 9, 12, 9, 4],
+                                  [2, 4, 5, 4, 2]])
+        y_kern_weights = np.transpose(x_kern_weights)
+        k1 = Kernel(x_kern_weights, 1/159)
+        self.update_kernel(k1)
+        k2 = Kernel(y_kern_weights, 1/159)
+        Gx = apply_conv(self.image, k1, self.grey)
+        Gy = apply_conv(self.image, k2, self.grey)
+        G = np.sqrt(Gx*Gx + Gy*Gy)
+        self.image = np.clip(G, 0, 255).astype(int)
+        self.redraw(self.grey)
+
+    def prewitt(self):
+        if not self.grey:
+            self.image = np.expand_dims(np.mean(self.image, axis=2), axis=2)
+        x_kern_weights = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
+        y_kern_weights = np.transpose(x_kern_weights)
+        k1 = Kernel(x_kern_weights)
+        k2 = Kernel(y_kern_weights)
+        self.update_kernel(k1)
+        Gx = apply_conv(self.image, k1, self.grey)
+        Gy = apply_conv(self.image, k2, self.grey)
+        G = np.sqrt(Gx*Gx + Gy*Gy)
+        if not self.grey:
+            self.image = np.mean(self.image, axis=2)
+        self.image = np.clip(G, 0, 255).astype(int)
+        if not self.grey:
+            self.image = np.squeeze(self.image, axis=2)
+        self.redraw(grey=True)
 
     def custom_kernel(self):
         w = self.weights_from_entries()
         coeff = float(self.main_mult.get())
         kernel = Kernel(w, coeff)
-        self.image = apply_conv(self.image, kernel)
-        self.redraw()
+        self.image = apply_conv(self.image, kernel, self.grey)
+        self.redraw(self.grey)
 
     def edgedetect(self):
+        if not self.grey:
+            self.image = np.expand_dims(np.mean(self.image, axis=2), axis=2)
         kernel = Kernel(np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]),
                         1.)
         self.update_kernel(kernel)
-        self.image = apply_conv(self.image, kernel)
-        self.redraw()
+        self.image = apply_conv(self.image, kernel, self.grey)
+        if not self.grey:
+            self.image = np.squeeze(self.image, axis=2)
+        self.redraw(grey=True)
 
     def get_entry(self, index):
         return float(self.entries[index].get())
 
+    def load_img(self):
+        entry = self.entry_file.get()
+        try:
+            self.image = load_image(entry)
+            self.filename = entry
+            self.grey = False
+            self.redraw()
+        except ValueError as e:
+            print(e)
+            print('The entry for the path is probably empty.')
+        except FileNotFoundError:
+            print('Invalid filename for loading the image.')
+
+    def save_img(self):
+        filename = self.entry_saveas.get()
+        try:
+            path = os.path.join('images\\', filename)
+            io.imsave(path, self.image.astype(np.uint8))
+            print('File saved as {}'.format(path))
+        except TypeError:
+            print('Wrong filename for saving.')
+        except ValueError:
+            print('Wrong filename for saving')
+
+    def to_grey(self):
+        if self.grey:
+            return
+        self.grey = True
+        self.image = np.mean(self.image, axis=2)
+        self.redraw(self.grey)
+
+    def to_corrected_grey(self):
+        if self.grey:
+            return
+        self.grey = True
+        self.image = np.apply_along_axis(corrected_grey, 2, self.image)
+        self.redraw(self.grey)
+
     def weights_from_entries(self):
         w = np.array([])
-        for i in range(9):
+        for i in range(25):
             w = np.append(w, self.get_entry(i))
-        w = w.reshape((3, 3))
+        w = w.reshape((5, 5))
         return w
 
     def gaussianblur(self):
         kernel = Kernel(np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]), 1/19)
         self.update_kernel(kernel)
-        self.image = apply_conv(self.image, kernel)
-        self.redraw()
+        self.image = apply_conv(self.image, kernel, self.grey)
+        self.redraw(self.grey)
+
+    def roberts(self):
+        if not self.grey:
+            self.image = np.expand_dims(np.mean(self.image, axis=2), axis=2)
+        k1 = Kernel(np.array([[1, 0], [0, -1]]), 1.)
+        k2 = Kernel(np.array([[0, 1], [-1, 0]]), 1.)
+        self.update_kernel(k1)
+        Gx = apply_conv(self.image, k1, self.grey)
+        Gy = apply_conv(self.image, k2, self.grey)
+        G = np.sqrt(Gx*Gx + Gy*Gy)
+        self.image = np.clip(G, 0, 255)
+        if not self.grey:
+            self.image = np.squeeze(self.image, axis=2).astype(int)
+        self.redraw(grey=True)
 
     def sharpen(self):
         kernel = Kernel(np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]), 1.)
         self.update_kernel(kernel)
-        self.image = apply_conv(self.image, kernel)
-        self.redraw()
+        self.image = apply_conv(self.image, kernel, self.grey)
+        self.redraw(self.grey)
 
     def sobel(self):
-        self.image = np.expand_dims(np.mean(self.image, axis=2), axis=2)
+        if not self.grey:
+            self.image = np.expand_dims(np.mean(self.image, axis=2), axis=2)
         k1 = Kernel(np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]), 1.)
         k2 = Kernel(np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]), 1.)
-        Gx = apply_conv(self.image, k1)
-        Gy = apply_conv(self.image, k2)
+        self.update_kernel(k1)
+        Gx = apply_conv(self.image, k1, self.grey)
+        Gy = apply_conv(self.image, k2, self.grey)
         G = np.sqrt(Gx*Gx + Gy*Gy)
         self.image = np.clip(G, 0, 255)
+        if not self.grey:
+            self.image = np.squeeze(self.image, axis=2)
         self.redraw(grey=True)
 
     def update_kernel(self, kernel):
-        w = kernel.weights
+        w = kernel.to_5x5()
         coeff = kernel.coeff
-        for i in range(3):
-            for j in range(3):
-                self.entries[i+j*3].delete(0, 'end')
-                self.entries[i+j*3].insert(0, w[j, i])
+        for i in range(5):
+            for j in range(5):
+                self.entries[i+j*5].delete(0, 'end')
+                self.entries[i+j*5].insert(0, str(w[j, i]))
         self.main_mult.delete(0, 'end')
         self.main_mult.insert(0, coeff)
 
     def init_entries(self):
         list_entries = []
 
-        for i in range(3):
-            for j in range(3):
+        for i in range(5):
+            for j in range(5):
                 entry = tk.Entry(self.root)
-                if i == 1 and j == 1:
+                if i == 2 and j == 2:
                     entry.insert(0, '1')
                 else:
                     entry.insert(0, '0')
@@ -264,8 +478,14 @@ class Display():
         return list_entries
 
     def reset_image(self):
-        self.image = load_image('test.jpg')
-        self.redraw()
+        self.image = load_image(self.filename)
+        self.slider_red.set(1)
+        self.slider_values[0] = 1.
+        self.slider_green.set(1)
+        self.slider_values[1] = 1.
+        self.slider_blue.set(1)
+        self.slider_values[2] = 1.
+        self.redraw(grey=False)
 
     def reset_kernel(self):
         kernel = Kernel()
@@ -287,7 +507,7 @@ class Display():
         self.update_colors(None)
 
     def update_colors(self, event):
-        self.image = load_image('test.jpg')
+        self.image = load_image(self.filename)
         self.slider_values[0] = self.slider_red.get()
         self.slider_values[1] = self.slider_green.get()
         self.slider_values[2] = self.slider_blue.get()
@@ -298,12 +518,13 @@ class Display():
         self.redraw()
 
     def redraw(self, grey=False):
-        if grey:
-            self.a.imshow(self.image, cmap='Greys')
+        self.grey = grey
+        if self.grey:
+            self.a.imshow(self.image, cmap='gray')
         else:
             self.a.imshow(self.image)
         self.canvas.draw()
 
 
 if __name__ == '__main__':
-    display = Display('test.jpg')
+    display = Display('test2.jpg')
